434e39e8ce8966cbd516df5422faa2f313127900
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-11-30 18:55:24 -0500

    first commit

---
+++ Assets/Activator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    // public KeyCode key;
+    bool active = false;
+    GameObject note;
+    KeyCode noteKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(noteKey) && active) {
+            Destroy(note);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        active = true;
+        if (col.gameObject.tag == "Note") {
+            note = col.gameObject;
+            noteKey = col.GetComponent<Notes>().key;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        active = false;
+    }
+}
---
+++ Assets/Notes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float hSpeed;
+    public float vSpeed;
+    public KeyCode key;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // Debug.Log("key needed: " + key);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector2(hSpeed, vSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
